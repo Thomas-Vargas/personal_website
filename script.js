9694a8d29ff886fa325b5e5b81a87d2501839841
Thomas Vargas <tavargas2012irc@gmail.com>
2022-04-07 16:00:23 -0500

    portfolio in its own directory

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let current_url = document.location
 
 document.querySelectorAll(".changeable").forEach(function(e){
     if(e.href == current_url){
+        console.log(e.href)
        e.classList += " current";
     }
 })
