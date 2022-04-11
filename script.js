8b9fd0b6574c8d7fe03fa47208878dfdf5d1baaa
Thomas Vargas <tavargas2012irc@gmail.com>
2022-04-11 13:24:30 -0500

    Fixed JS for nav link selection.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let current_url = document.location
 
 document.querySelectorAll(".changeable").forEach(function(e){
     if(e.href == current_url){
-        console.log(e.href)
-        e.classList += "current";
+        e.classList.add("current")
     }
 })
