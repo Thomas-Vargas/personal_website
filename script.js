5f4e2be40e4ccb7e611fc56ddbe3e61f827c505b
Thomas Vargas <tavargas2012irc@gmail.com>
2022-04-11 13:08:14 -0500

    Logo for portfolio tab fixed.

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let current_url = document.location
 document.querySelectorAll(".changeable").forEach(function(e){
     if(e.href == current_url){
         console.log(e.href)
-       e.classList += " current";
+        e.classList += "current";
     }
 })
